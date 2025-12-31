3a529c48bb082bf4e05112303a569f3b8c60f152
Yash Jambure <yashjambure007@gmail.com>
2025-12-31 13:24:07 +0530

    updated

--- Frontend/src/components/Chatbox.jsx
+++ Frontend/src/components/Chatbox.jsx
@@ -9,18 +9,16 @@ const Chatbox = ({ fetchAgain, setFetchAgain }) => {
   return (
     <Box
       display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
-      flexDirection="column"          // ✅ IMPORTANT (not alignItems)
+      flexDirection="column"    
       p={3}
       bg="rgba(10,14,20,0.85)"
       w={{ base: "100%", md: "68%" }}
 
-      /* ✅ SAME HEIGHT AS SIDEBAR */
-      h="100%"                        // 🔥 KEY FIX (not vh here)
+      h="100%"                  
 
       borderRadius="2xl"
       boxShadow="0 18px 40px rgba(2,6,23,0.65)"
 
-      /* ✅ PREVENT CHAT FROM EXPANDING */
       overflow="hidden"
     >
       <SingleChat
