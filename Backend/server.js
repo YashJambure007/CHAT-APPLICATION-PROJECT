d902fc1c706b24219522619100d1361a06f9d9f2
Yash Jambure <yashjambure007@gmail.com>
2025-12-31 13:28:29 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -24,21 +24,16 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow server-to-server & Postman
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("CORS not allowed"));
-      }
-    },
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+// IMPORTANT: explicitly handle preflight
+app.options("*", cors());
+
 
 // ================= ROUTES =================
 app.use("/api/user", userRoutes);
